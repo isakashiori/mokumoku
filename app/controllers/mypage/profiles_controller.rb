c26fddd3797f7e2e4066dfc9ccbd59eb6063ec85
isakashiori <isslamdunk0000@gmail.com>
2022-12-06 21:06:35 +0900

    fix:Rspecの結果について

--- app/controllers/mypage/profiles_controller.rb
+++ app/controllers/mypage/profiles_controller.rb
@@ -12,20 +12,16 @@ class Mypage::ProfilesController < Mypage::BaseController
   def update
     if params[:user_id] == nil
       @user = User.find(current_user.id)
-      if @user.update(profile_params)
-        redirect_to mypage_profile_path, success: 'プロフィールを更新しました'
-      else
-        render :show
-      end
+        if @user.update(profile_params)
+          redirect_to mypage_profile_path, success: 'プロフィールを更新しました'
+        else
+          render :show
+        end
     else
-      @user = User.find(params[:user_id])
-      if @user.update(profile_params)
-        redirect_to mypage_profile_path(user_id: params[:user_id]), success: 'プロフィールを更新しました'
-      else
-        render :show
-      end
+      テスト
     end
   end
+
 
   private
 
